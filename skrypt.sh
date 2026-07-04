561eb8bea1d90da7601fcb13b4c530ec34fb40e9
Bartłomiej Beczek <dh.beczus@gmail.com>
2026-07-04 14:54:59 +0200

    Dodano tworzenie 100 plikow log

--- skrypt.sh
+++ skrypt.sh
@@ -6,11 +6,30 @@ show_date() {
     date +"%Y-%m-%d"
 }
 
+create_logs() {
+    COUNT=100
+
+    for ((i=1; i<=COUNT; i++))
+    do
+        FILE_NAME="log${i}.txt"
+        {
+            echo "Nazwa pliku: $FILE_NAME"
+            echo "Utworzony przez skrypt: $SCRIPT_NAME"
+            echo "Data utworzenia: $(date +"%Y-%m-%d %H:%M:%S")"
+        } > "$FILE_NAME"
+    done
+
+    echo "Utworzono $COUNT plików log."
+}
+
 case "$1" in
     --date)
         show_date
         ;;
+    --logs)
+        create_logs
+        ;;
     *)
-        echo "Nieznana opcja. UĹĽyj: ./skrypt.sh --help"
+        echo "Nieznana opcja. Użyj: ./skrypt.sh --help"
         ;;
 esac
